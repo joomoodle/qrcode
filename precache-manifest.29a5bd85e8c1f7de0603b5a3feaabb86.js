cd2b6c7b3838179967fc0464e48cd7453ec1eb82
joomoodle <juan_pablo_ing@outlook.es>
2020-02-27 12:35:58 -0600

    Updates

---
+++ precache-manifest.29a5bd85e8c1f7de0603b5a3feaabb86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98dcee76ea1a020618b3ee872a51521c",
+    "url": "/qrcode/index.html"
+  },
+  {
+    "revision": "16f8dd9c64076c8f1e60",
+    "url": "/qrcode/static/css/main.1dd50e88.chunk.css"
+  },
+  {
+    "revision": "36d8cdf30460222b5a40",
+    "url": "/qrcode/static/js/2.4eef57c7.chunk.js"
+  },
+  {
+    "revision": "0f2b7da9b8b92c3ae78de2c0794146b8",
+    "url": "/qrcode/static/js/2.4eef57c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f8dd9c64076c8f1e60",
+    "url": "/qrcode/static/js/main.a0a07d16.chunk.js"
+  },
+  {
+    "revision": "affc020c9b28f7987796",
+    "url": "/qrcode/static/js/runtime-main.15721cb2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/qrcode/static/media/logo.5d5d9eef.svg"
+  }
+]);
